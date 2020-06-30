f4d575c2eabb2b5574b8ee4615fd759505e70d0c
Kosi Akporji <kosiazom@gmail.com>
2020-06-30 12:55:34 -0400

    started company views and controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,40 @@
 class CompaniesController < ApplicationController
+    
+    def index
+        @companies = Company.all
+    end
+    
+    
+    def show
+        find_company
+    end
+    
+    def new
+        @company = Company.new
+    end
+    
+    
+    def create
+        @company = Company.create(company_params)
+        redirect_to company_path(@company)
+    end
+
+    def new_employee
+        find_company
+        @employee = Employee.new
+    
+    end
+    
+
+    private
+
+    def find_company
+       @company = Company.find(params[:id]) 
+    end
+
+    def company_params
+        params.require(:company).permit(:name)
+    end
+    
+    
 end
