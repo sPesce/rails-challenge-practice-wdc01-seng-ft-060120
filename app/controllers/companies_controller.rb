4ec431a1905760cc3ded0481050451546de9a2c4
steve <stevepesce879@gmail.com>
2020-06-30 18:38:16 -0400

    finished CompaniesController#create,added notes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -18,10 +18,30 @@ class CompaniesController < ApplicationController
     def create
 
         @company = Company.new(company_params)
+        byebug
+        #if company is invalid, it wont save
         if @company.save
-       # byebug
-        params[:company][:offices_attributes].each 
-        redirect_to company_path(@company)
+          #block paramaters of the hash[key] => value
+          #this block breaks the hash into seperate hashes for each nested hash of office_attributes
+          params[:company][:offices_attributes].each do |key,value|           
+            #each 'value' is a hash describing one (to be created) office,
+            #where [:id] is the building id, and for some reason, [:offices] is the 
+            # an array holding each floor number selected
+            value[:offices].each do |floor|
+              #if not selected, its value is just ""
+              if floor != ""
+                #using original params here, but using the 'key' I defined in the block
+                  #  because this key shows which (to be created)office id we are currently 
+                  #  iterating through 
+                new_office = Office.new(                  
+                  building_id: params[:company][:offices_attributes][key][:id],
+                  company_id: @company.id,
+                  floor: floor)
+                new_office.save
+              end             
+            end
+          end  
+          redirect_to company_path(@company)
         else
             render :new
         end
---
+++ example_of_params.md
@@ -0,0 +1,20 @@
+#####:= unused param, we dont use it, leaving it out to avoid confusion
+
+{
+  "#####",
+ "#####",
+ "company"=><ActionController::Parameters {
+                                            "name" => "Bobs Construction",
+                                                   "offices_attributes"=>{
+                                                                            "0"=>{
+                                                                                  "id"=>"1", "offices"=>["","6"]
+                                                                                },
+                                                                            "1"=>{
+                                                                                    "id"=>"2",
+                                                                                    "offices"=>["","1"]
+                                                                                  },
+                                                                            "2"=>{
+                                                                              "id"=>"3", 
+                                                                              "offices"=> ["","5"]
+                                                                          }
+} #####, "#####"=>"#####", "######", "#####"}
